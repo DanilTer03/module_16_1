455aaf4f88f728a955ce981bfa83dc9eb72b8aad
Danil Terentev <danil.terentyev@gmail.com>
2024-11-18 14:16:39 +0300

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -8,16 +8,16 @@ async def main() -> dict:
     return {"message": "Главная страница"}
 
 
-@app.get("/user")
-async def user() -> dict:
-    return {"message": "Информация о пользователе. Имя: <username>, Возраст: <age>"}
-
-
 @app.get("/user/admin")
 async def admin() -> dict:
     return {"message": "Вы вошли как администратор"}
 
 
-@app.get("/user/[{user_id}")
+@app.get("/user/{user_id}")
 async def user(user_id: int) -> dict:
     return {"message": f"Вы вошли как пользователь №{user_id}"}
+
+
+@app.get("/user")
+async def user() -> dict:
+    return {"message": "Информация о пользователе. Имя: <username>, Возраст: <age>"}
